3281cf4c44f9e700f1cbb33b3e64c1eccc0bbe49
ArchawinL <a.leelapanang@gmail.com>
2025-09-24 23:00:33 +0100

    Transformer complete

---
+++ README.md
@@ -0,0 +1,5 @@
+# Recreation of Transformer using Pytorch
+
+References:
+
+- [Data Camp Tutorial](https://www.datacamp.com/tutorial/building-a-transformer-with-py-torch)
--- transformer.py
+++ transformer.py
@@ -94,9 +94,9 @@ class PositionalEncoding(nn.Module):
         return x + self.pe[:, :x.size(1)]
 
 
-class EncodeLayer(nn.Module):
+class EncoderLayer(nn.Module):
     def __init__(self, d_model, num_heads, d_ff, dropout):
-        super(EncodeLayer, self).__init__()
+        super(EncoderLayer, self).__init__()
         self.self_attn = MultiHeadAttention(d_model, num_heads)
         self.feed_forward = PositionWiseFeedForward(d_model, d_ff)
         self.norm1 = nn.LayerNorm(d_model) 
@@ -130,6 +130,51 @@ class DecoderLayer(nn.module):
 
         return x
 
+
+class Transformer(nn.Module):
+    def __init__(self, src_vocab_size, tgt_vocab_size, d_model, num_heads, num_layers, d_ff, max_seq_length, dropout):
+        super(Transformer, self).__init__()
+        # nn.Embedding creates an essential look up table, encode meaning or relationship between words.
+        self.encoder_embedding = nn.Embedding(src_vocab_size, d_model)
+        self.decoder_embedding = nn.Embedding(tgt_vocab_size, d_model)
+        self.positional_encoding = PositionalEncoding(d_model, max_seq_length)
+
+        # nn.ModuleList just defines a list without connection as opposed to nn.Sequential. Makes PyTorch aware of the list.
+        self.encoder_layers = nn.ModuleList([EncoderLayer(d_model, num_heads, d_ff, dropout) for _ in range(num_layers)])
+        self.decoder_layers = nn.ModuleList([DecoderLayer(d_model, num_heads, d_ff, dropout) for _ in range(num_layers)])
+
+        self.fc = nn.Linear(d_model, tgt_vocab_size)
+        self.dropout = nn.Dropout(dropout)
+
+    def generate_mask(self, src, tgt):
+        # Tells model to ignore padding tokens, in encoder phase
+        src_mask = (src != 0).unsqueeze(1).unsqueeze(2)
+        
+        # Stops model from looking ahead in the sequence while predicting current ones, in decoder phase
+        tgt_mask = (tgt != 0).unsqueeze(1).unsqueeze(3)
+        seq_length = tgt.size(1)
+        nopeak_mask = (1 - torch.triu(torch.ones(1, seq_length, seq_length), diagonal=1)).bool()
+        # Combining the masks
+        tgt_mask = tgt_mask & nopeak_mask
+        return src_mask, tgt_mask
+    
+    def forward(self, src, tgt):
+        src_mask, tgt_mask = self.generate_mask(src, tgt)
+        src_embedded = self.dropout(self.positional_encoding(self.encoder_embedding(src)))
+        tgt_embedded = self.dropout(self.positional_encoding(self.decoder_embedding(tgt)))
+
+        enc_output = src_embedded
+        for enc_layer in self.encoder_layers:
+            enc_output = enc_layer(enc_output, src_mask)
+
+        dec_output = tgt_embedded
+        for dec_layer in self.decoder_layers:
+            dec_output = dec_layer(dec_output, enc_output, src_mask, tgt_mask)
+
+        output = self.fc(dec_output)
+        return output
+        
+    
 
 
 print(torch.cuda.is_available())
